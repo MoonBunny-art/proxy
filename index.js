f7b749ceb4b4ad79e62b8387f21775a0f7f0beb6
MoonBunny-art <moon.bunny043@gmail.com>
2026-03-04 17:09:01 +0100

    Block browser requests and allow executors access

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const http = require('http');
 const https = require('https');
+
 const REAL_URL = "https://raw.githubusercontent.com/MoonBunny-art/script.lua2/bae296cc06a66ae3f99377401d797322058cd66e/script.lua2";
+
 http.createServer((req, res) => {
   const ua = (req.headers['user-agent'] || '').toLowerCase();
-  const ok = ["synapse","krnl","fluxus","delta","wave","evon","hydrogen"].some(e=>ua.includes(e));
-  if (ok) { https.get(REAL_URL, r => r.pipe(res)); }
-  else { res.writeHead(404); res.end("JUNKIE SYSTEM\n\nerror:404"); }
+  
+  // Browser blockieren
+  const isBrowser = ua.includes("mozilla") || ua.includes("chrome") || ua.includes("safari") || ua.includes("firefox") || ua.includes("edge");
+  
+  if (isBrowser) {
+    res.writeHead(404);
+    res.end("JUNKIE SYSTEM\n\nerror:404");
+  } else {
+    // Alles andere (Executors) bekommt das Script
+    https.get(REAL_URL, r => r.pipe(res));
+  }
 }).listen(process.env.PORT || 3000);
