5553f0a78371df96705417d78f71b593e4d2e14f
MoonBunny-art <moon.bunny043@gmail.com>
2026-03-04 17:14:00 +0100

    Implement secret key validation for requests
    Added a secret key check to the request handler.

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ const http = require('http');
 const https = require('https');
 
 const REAL_URL = "https://raw.githubusercontent.com/MoonBunny-art/script.lua2/bae296cc06a66ae3f99377401d797322058cd66e/script.lua2";
+const SECRET = "moonbunny123";
 
 http.createServer((req, res) => {
-  const ua = (req.headers['user-agent'] || '').toLowerCase();
-  
-  // Browser blockieren
-  const isBrowser = ua.includes("mozilla") || ua.includes("chrome") || ua.includes("safari") || ua.includes("firefox") || ua.includes("edge");
-  
-  if (isBrowser) {
+  if (req.url === "/" + SECRET) {
+    https.get(REAL_URL, r => r.pipe(res));
+  } else {
     res.writeHead(404);
     res.end("JUNKIE SYSTEM\n\nerror:404");
-  } else {
-    // Alles andere (Executors) bekommt das Script
-    https.get(REAL_URL, r => r.pipe(res));
   }
 }).listen(process.env.PORT || 3000);
